00de769153c530f5c51c3f5e601b36750c1e1427
Lalalalex <a31294943@gmail.com>
2024-06-13 05:06:36 +0800

    iloc bug fix

--- Final/datareader_category.py
+++ Final/datareader_category.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 import numpy as np
+from sklearn.metrics import roc_curve, auc
 
 class Dataset(Dataset):
     def __init__(self, behaviors_path, news_path, embeddings_parh, is_test_model = False):
@@ -39,15 +40,15 @@ class Dataset(Dataset):
         return df
     
     def __getitem__(self, index):
-        id = int(self.behaviors_df['id'][index])
-        clicked_news = self.behaviors_df['clicked_news'][index].split()
+        id = int(self.behaviors_df.iloc[index]['id'])
+        clicked_news = self.behaviors_df.iloc[index]['clicked_news'].split()
         clicked_news_num = len(clicked_news)
         clicked_news_infos = []
         for news in clicked_news:
             clicked_news_infos.append(self.get_news_info(news))
 
         if self.is_test_model:
-            impression_news = self.behaviors_df['impressions'][index].split()
+            impression_news = self.behaviors_df.iloc[index]['impressions'].split()
             impression_news_infos = []
             for news in impression_news:
                 impression_news_infos.append(self.get_news_info(news))
@@ -61,7 +62,7 @@ class Dataset(Dataset):
                 'clicked_news_num': clicked_news_num
             }
 
-        impressions_all = self.behaviors_df['impressions'][index].split()
+        impressions_all = self.behaviors_df.iloc[index]['impressions'].split()
         impression_news = [impressions_data.split('-')[0] for impressions_data in impressions_all]
         impression_news_infos = []
         for news in impression_news:
@@ -82,3 +83,54 @@ cfg = Config()
 train_dataset = Dataset(cfg.train_behaviors_path, cfg.train_news_path, cfg.train_embeddings_path)
 test_dataset = Dataset(cfg.test_behaviors_path, cfg.test_news_path, cfg.test_embeddings_path, is_test_model = True)
 
+def train(dataset):
+    total_accuracy = 0
+    with tqdm(dataset, unit = 'data', desc = 'Train') as tqdm_loader:
+        for index, data in enumerate(tqdm_loader):
+            clicked_news_infos = data['clicked_news_infos']
+            impression_news_infos = data['impression_news_infos']
+            label = data['impression_labels']
+
+            predict = [1 for i in range(len(impression_news_infos))]
+
+            for i, impression_news in enumerate(impression_news_infos):
+                for clicked_news in clicked_news_infos:
+                    if clicked_news['category'] == impression_news['category']:
+                        break
+                    predict[i] = 0
+            
+            fpr, tpr, _ = roc_curve(label, predict)
+            accuracy = auc(fpr, tpr)
+            total_accuracy = total_accuracy + accuracy
+            average_accuracy = total_accuracy/(index + 1)
+            tqdm_loader.set_postfix(average_accuracy = average_accuracy, accuracy = accuracy)
+
+def test(dataset, sample_df):
+    total_accuracy = 0
+    predict_list = []
+    with tqdm(dataset, unit = 'data', desc = 'Test') as tqdm_loader:
+        for index, data in enumerate(tqdm_loader):
+            id = data['id']
+            clicked_news_infos = data['clicked_news_infos']
+            impression_news_infos = data['impression_news_infos']
+
+            predict = [1 for i in range(len(impression_news_infos))]
+
+            for i, impression_news in enumerate(impression_news_infos):
+                for clicked_news in clicked_news_infos:
+                    if clicked_news['category'] == impression_news['category']:
+                        break
+                    predict[i] = 0
+            
+            sample_df.loc[index, 'id'] = id
+            for i in range(15):
+                sample_df.loc[index, 'p' + str(i + 1)] = predict[i]
+
+            predict_list.append(predict)
+    
+    return sample_df
+
+# train(train_dataset)
+sample_df = pd.read_csv(cfg.sample_submission_path)
+sample_df = test(test_dataset, sample_df)
+sample_df.to_csv(os.path.join('./', 'submission.csv'), index = False)
